9f74ef328ea7e0fb3721707f8370fcffa2497e63
Gavin Laking <gavinlaking@gmail.com>
2016-10-12 11:44:49 +0100

    Introduce 'Render' class;
    Render will take a string buffer and write it to the screen
    after first clearing and returning the cursor to position 1,1.

--- src/cod.cpp
+++ src/cod.cpp
@@ -5,8 +5,26 @@ using namespace std;
 const string ESC_CLEAR_SCREEN = "\e[2J";
 const string ESC_RESET_CURSOR = "\e[1;1H";
 
+class Render {
+    string buffer;
+  public:
+    Render(string);
+
+    int perform() {
+      cout << ESC_CLEAR_SCREEN;
+      cout << ESC_RESET_CURSOR;
+      cout << buffer;
+      return 0;
+    }
+};
+
+Render::Render(string str) {
+  buffer = str;
+}
+
 int main() {
-  cout << ESC_CLEAR_SCREEN + ESC_RESET_CURSOR + "Hello world!\n";
+  Render render("Hello world!\n");
+  render.perform();
   return 0;
 }
 
