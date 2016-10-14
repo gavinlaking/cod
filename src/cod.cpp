46aa4f3252ce021c38176c4a80776a9b7cf91c3f
Gavin Laking <gavinlaking@gmail.com>
2016-10-14 16:08:13 +0100

    Introduce 'Cursor' class;
    The Cursor class will store the position of, and allow
    the manipulation of the cursor class.

--- src/cod.cpp
+++ src/cod.cpp
@@ -16,6 +16,56 @@ class Buffer {
     }
 };
 
+class Cursor {
+  public:
+    Cursor(int n, int m) {
+      cy = n;
+      cx = m;
+    }
+    Cursor(int n) {
+      cy = n;
+      cx = 1;
+    }
+    Cursor() {
+      cy = 1;
+      cx = 1;
+    }
+
+    int cy;
+    int cx;
+
+    void down() {
+      cy++;
+    }
+    void inspect() {
+      cout << " cx: " << cx << " cy: " << cy << endl;
+    }
+    void left() {
+      if (cx <= 0) {
+        cx = 1;
+      } else if (cx - 1 < 1) {
+        cx = 1;
+      } else {
+        cx--;
+      }
+    }
+    void render() {
+      cout << "\e[" << cy << ";" << cx << "H";
+    }
+    void right() {
+      cx++;
+    }
+    void up() {
+      if (cy <= 0) {
+        cy = 1;
+      } else if (cy - 1 < 1) {
+        cy = 1;
+      } else {
+        cy--;
+      }
+    }
+};
+
 class Render {
     string buffer;
   public:
