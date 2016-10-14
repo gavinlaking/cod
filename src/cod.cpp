5689e7c479a41a53bc550d27b7e8302c2428198b
Gavin Laking <gavinlaking@gmail.com>
2016-10-14 15:39:45 +0100

    Introduce 'Terminal' class;
    Terminal will hold the current size of the terminal interface.

--- src/cod.cpp
+++ src/cod.cpp
@@ -28,8 +28,30 @@ class Render {
     }
 };
 
+class Terminal {
+  public:
+    Terminal() {
+      tx = 1;
+      ty = 1;
+      txn = 80;
+      tyn = 25;
+    }
+
+    int tx;
+    int ty;
+    int txn;
+    int tyn;
+};
+
 int main() {
   Render render("Hello world!\n");
   render.perform();
+
+  int i = 0;
+  while(i < 20) {
+    cout << i << endl;
+    i++;
+  }
+
   return 0;
 }
